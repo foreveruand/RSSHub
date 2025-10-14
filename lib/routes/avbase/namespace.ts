2eb6ef01b59e7915f9a37f113be1d6d2c927622b
foreveruand <43624948+foreveruand@users.noreply.github.com>
2025-10-14 14:48:46 +0800

    Create avbase

---
+++ lib/routes/avbase/namespace.ts
@@ -0,0 +1,9 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'AVbase',
+    url: 'www.avbase.net',
+    description: `::: tip
+:::`,
+    lang: 'ja',
+};
